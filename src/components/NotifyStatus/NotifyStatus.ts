4a77ffd890ad0424258c0d514f462ed2bd3307c7
AndreiReznikov <andrezniksk@yandex.ru>
2026-03-02 00:04:04 +0300

    added notify statuses

---
+++ src/components/NotifyStatus/NotifyStatus.ts
@@ -0,0 +1,54 @@
+import successImage from "../../assets/statuses/success.png";
+import errorImage from "../../assets/statuses/error.png";
+import notPayedImage from "../../assets/statuses/not-payed.png";
+
+const notifyStatusStyles = new CSSStyleSheet();
+
+notifyStatusStyles.replaceSync(`
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .text {
+    font-weight: 600;
+    font-size: 24px;
+    color: #595959;
+  }
+`);
+
+const status = "success";
+const image =
+  status === "success"
+    ? successImage
+    : status === "error"
+      ? errorImage
+      : status === "not-payed"
+        ? notPayedImage
+        : "";
+
+class CNotifyStatus extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+
+    if (!this.shadowRoot) return;
+
+    const text = this.getAttribute("text") || "";
+
+    this.shadowRoot.adoptedStyleSheets = [notifyStatusStyles];
+    this.shadowRoot.innerHTML = `
+      <div class="container">
+        <img class="image" width="88" height="auto" src="${image}" alt="${text}">
+        <h2 class="text">${text}</h2>
+        <slot></slot>
+      </div>
+    `;
+  }
+}
+
+customElements.define("c-notify-status", CNotifyStatus);
+
+export default CNotifyStatus;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./components/PaymentForm";
 import "./components/UnipayForm";
 import "./components/Footer";
+import "./components/NotifyStatus";
 import "./ui/Button";
 import "./ui/Checkbox";
 import "./ui/Input";
