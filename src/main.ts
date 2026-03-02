033b5522ddc3463190ffa67a852224a68c25f7a0
AndreiReznikov <andrezniksk@yandex.ru>
2026-03-02 11:19:32 +0300

    divided imports into pages

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,1 @@
-import "@components/PaymentForm";
-import "@components/UnipayForm";
-import "@components/Footer";
-import "@components/NotifyStatus";
-import "@ui/Button";
-import "@ui/Checkbox";
-import "@ui/Input";
-import "@ui/Textarea";
-import "@ui/HelpIcon";
-
 import "./style.css";
